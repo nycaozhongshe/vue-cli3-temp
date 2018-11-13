f779b9f132659713ef4c52113dde364dc6982d74
1 <czs1037976103@gmail.com>
2018-11-13 21:55:29 +0800

    no message

--- src/views/Contacts/Index.vue
+++ src/views/Contacts/Index.vue
@@ -16,8 +16,40 @@ var demoEvents = [
     title: 'Sunny Out of Office',
     start: '2018-10-25',
     end: '2018-11-11',
-    color: 'red',
-    editable: true
+    color: 'red'
+  },
+  {
+    title: 'Sunny Out of Office',
+    start: '2018-10-25',
+    end: '2018-11-11',
+    color: 'red'
+  },
+  {
+    title: 'Sunny Out of Office',
+    start: '2018-10-25',
+    end: '2018-11-11',
+    color: 'red'
+  },
+  {
+    title: 'Sunny Out of Office',
+    start: '2018-10-25',
+    end: '2018-11-11',
+    color: 'red'
+  }, {
+    title: 'Sunny Out of Office',
+    start: '2018-10-25',
+    end: '2018-11-11',
+    color: 'red'
+  }, {
+    title: 'Sunny Out of Office',
+    start: '2018-10-25',
+    end: '2018-11-11',
+    color: 'red'
+  }, {
+    title: 'Sunny Out of Office',
+    start: '2018-10-25',
+    end: '2018-11-11',
+    color: 'red'
   }
 ]
 export default {
@@ -36,8 +68,27 @@ export default {
       fcEvents: demoEvents,
       config: {
         locale: 'zh-cn',
+        defaultView: 'month',
+        header: {
+          left: 'prev,next today', // 上一页、下一页、今天
+          center: 'title', // 居中：时间范围区间标题
+          right: 'myCustomButton,month,agendaWeek,agendaDay,listMonth' // 右边：显示哪些视图
+        },
+        customButtons: { // 自定义header属性中按钮[customButtons与header并用]
+          myCustomButton: {
+            text: '新建',
+            click: function () {
+              alert('clicked the custom button!')
+            }
+          }
+        },
+        eventLimit: true,
+        eventLimitText: '更多',
+        weekNumbers: true,
+        fixedWeekCount: true,
         // aspectRatio: 1,
         weekends: true,
+        noEventsMessage: '没数据啊',
         drop (...args) {
           // handle drop logic in parent
         }
