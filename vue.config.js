d87e8ba203d83b5e03a11eb3840deca8e49ed89c
1 <czs1037976103@gmail.com>
2018-11-23 11:53:15 +0800

    iframe 页面

--- vue.config.js
+++ vue.config.js
@@ -27,7 +27,8 @@ const cdn = {
     'https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js',
     'https://cdn.jsdelivr.net/npm/vue-router@3.0.1/dist/vue-router.min.js',
     'https://cdn.jsdelivr.net/npm/vuex@3.0.1/dist/vuex.min.js',
-    'https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js'
+    'https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js',
+    '//at.alicdn.com/t/font_932341_f616vmjljgu.js'
   ]
 }
 // 需要预渲染的路由
