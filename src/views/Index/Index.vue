445e79f1e17582e9407908e2ce3dbda22d4bc4de
1 <czs1037976103@gmail.com>
2018-11-30 10:06:56 +0800

    测试一下

--- src/views/Index/Index.vue
+++ src/views/Index/Index.vue
@@ -1,7 +1,7 @@
 <!-- Index -->
 <template>
   <div>
-    <img v-lazy="{src:$baseUrl+'__imgs/common/timg1.jepg',loading:$baseUrl+'__imgs/common/timg1.jepg'}"
+    <img v-lazy="{src:$baseUrl+'__imgs/common/timg1.jpg',loading:$baseUrl+'__imgs/common/timg1.jpg'}"
          alt="">
 
     <el-input placeholder="请输入内容"
