c367f2833a568ee60a66c74b162b6e755076d5c5
1 <czs1037976103@gmail.com>
2018-12-02 21:24:53 +0800

    重置

--- src/App.vue
+++ src/App.vue
@@ -55,4 +55,24 @@ export default {
 // 全局font-size: 20px;
 // 变量$text-color、$theme-color
 // 和button()的mixin是借助sass-resources-loader实现的
+@media only screen and (min-width: 320px) {
+  html {
+    font-size: 62.5% !important;
+  }
+}
+@media only screen and (min-width: 640px) {
+  html {
+    font-size: 125% !important;
+  }
+}
+@media only screen and (min-width: 750px) {
+  html {
+    font-size: 150% !important;
+  }
+}
+@media only screen and (min-width: 1242px) {
+  html {
+    font-size: 187.5% !important;
+  }
+}
 </style>
